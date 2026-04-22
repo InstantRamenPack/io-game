7dc093c4840d9a7bce7e1a00d5a35e9d2a22bffe
Raymond Zhu <ray2009zhu@gmail.com>
2026-04-22 14:34:31 -0700

    make bullets have trail fx

--- apps/client/src/render/entity/projectile/CannonBulletRenderer.ts
+++ apps/client/src/render/entity/projectile/CannonBulletRenderer.ts
@@ -1,8 +1,8 @@
-import { CircleEntityRenderer } from "@client/render/entity/CircleEntityRenderer.ts";
 import type { EntityRendererOptions } from "@client/render/entity/EntityRenderer.ts";
+import { TrailProjectileRenderer } from "@client/render/entity/projectile/TrailProjectileRenderer.ts";
 import type { PixiRenderer } from "@client/render/PixiRenderer.ts";
 
-export class CannonBulletRenderer extends CircleEntityRenderer {
+export class CannonBulletRenderer extends TrailProjectileRenderer {
   constructor(pixiRenderer: PixiRenderer, options: EntityRendererOptions = {}) {
     super(pixiRenderer, 0xffb703, options);
   }
